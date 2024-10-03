f78e40864090721ea8099ce2ee49e2f20471d6e2
Zach Balder <zmbalder@gmail.com>
2024-10-03 15:01:45 -0400

    start writing unit tests for util functions

---
+++ src/util.test.ts
@@ -0,0 +1,74 @@
+import { assert, expect, test } from 'vitest';
+import { chooseRandoms, getChordName, notesToPianoKeys, translateProgression } from './util';
+
+test('chooses randoms', () => {
+  const items = [1, 2, 3, 4, 5]
+  const randoms = chooseRandoms(items, 3)
+  expect(randoms).toHaveLength(3)
+})
+
+test('chooses randoms with included item', () => {
+  const items = [1, 2, 3, 4, 5]
+  const randoms = chooseRandoms(items, 3, 2)
+  expect(randoms).toHaveLength(4)
+  expect(randoms).toContain(2)
+  expect(randoms.filter(item => item === 2)).toHaveLength(1)
+})
+
+test('chooses randoms with included items', () => {
+  const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  const randoms = chooseRandoms(items, 5, 3, 4)
+  expect(randoms).toHaveLength(7)
+  expect(randoms).toContain(3)
+  expect(randoms).toContain(4)
+  expect(randoms.filter(item => item === 3)).toHaveLength(1)
+  expect(randoms.filter(item => item === 4)).toHaveLength(1)
+})
+
+test('gets correct chord name', () => {
+  expect(getChordName('maj7', 'C#')).toBe('maj7');
+});
+
+test('gets correct chord name with inversion', () => {
+  expect(getChordName('/3', 'C#')).toBe('C#/E#');
+});
+
+test('gets correct chord name with another inversion', () => {
+  expect(getChordName('/5', 'C#')).toBe('C#/G#');
+});
+
+test('gets correct piano keys', () => {
+  assert.deepEqual(
+    notesToPianoKeys([
+      ['F4', 'A4', 'C5'],
+      ['Bb4', 'D5', 'F5'],
+      ['C5', 'Eb5', 'G5'],
+    ]),
+    [
+      [6, 10, 13],
+      [11, 15, 18],
+      [13, 16, 20],
+    ]
+  );
+});
+
+test('gets correct piano keys across 4 octaves', () => {
+  assert.deepEqual(
+    notesToPianoKeys([
+      ['F4', 'A4', 'C5'],
+      ['Bb3', 'D4', 'F4'],
+      ['Eb5', 'G5', 'C6'],
+    ]),
+    [
+      [18, 22, 25],
+      [11, 15, 18],
+      [28, 32, 37],
+    ]
+  );
+});
+
+test('translates progression', () => {
+  expect(translateProgression('I7, IId7, IIIm, VIa')).toBe(
+    'I7, iiᵒ7, iii, VI+'
+  );
+});
